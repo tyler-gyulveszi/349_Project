751b35bc112faf252dba61939cc944bb58ad2185
Tyler Gyulveszi <gyulves1@msu.edu>
2021-12-10 15:45:07 -0500

    Final Publication of MI349 Project

---
+++ interact.js
@@ -0,0 +1,14 @@
+
+function ButtonPress1() {
+    alert("Thank you for Signing Up, we look forward to your continued support!")
+
+}
+
+function ButtonPress2() {
+    alert("Thank you for signing in!")
+}
+
+function ButtonPress3() {
+    alert("Thank you for your feedback!")
+
+}
